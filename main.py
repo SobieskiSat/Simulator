8e3435eb5dae2140fa11781b1a16ee7b2ad3d7ed
TheRootOf3 <aceszablewski@gmail.com>
2019-04-22 22:11:57 +0200

    main structure update

--- main.py
+++ main.py
@@ -56,24 +56,24 @@ class Reader:
                 pass
 
 
-mesurments = {'x':50.5, 'y':19.5, 'h':504, 'temperature':23, 'pm10':50}
+mesurments = {'x':50.5, 'y':19.5, 'h':3000, 'temperature':23, 'pm10':50}
 
 c = CanSat(mesurments)
 def get_speial(id):
-    special = {'wx':10, 'wy':5}
+    special = {'wx':7, 'wy':5}
     return special[id]
 
 gps = Sensor('GPS')
 
-gps.add_measurement('x', 'd.x + d.wx*r(-0.00001, 0.00002)', 6)
+gps.add_measurement('x', 'd.x + d.wx*r(-0.00001, 0.00002)+r(-0.0003, 0.00016)', 6)
 gps.add_dependency('x', c.get_mesurments, 'x')
 gps.add_dependency('wx', get_speial, 'wx')
 
-gps.add_measurement('y', 'd.y + d.wy*r(-0.000002, 0.00004)', 6)
+gps.add_measurement('y', 'd.y + d.wy*r(-0.000002, 0.00004)+r(-0.00025, 0.00004)', 6)
 gps.add_dependency('y',c.get_mesurments, 'y')
 gps.add_dependency('wy', get_speial , 'wy')
 
-gps.add_measurement('h', 'd.h + r(-1, 0.1)', 2)
+gps.add_measurement('h', 'd.h + r(-5, 0.1)', 2)
 gps.add_dependency('h', c.get_mesurments, 'h')
 c.add_sensor(gps)
 
@@ -90,7 +90,9 @@ c.add_sensor(bmp)
 
 radio = Sensor('Radio')
 
-radio.add_measurement('rssi', 'r(30, 50)', 0)
+radio.add_measurement('rssi', 'r(30, 100)', 0)
+radio.add_measurement('send_num', 'r(0, 256)', 0) #nowe
+radio.add_measurement('battery', 'r(0, 100)', 0)
 c.add_sensor(radio)
 
 sps = Sensor('SPS')
@@ -99,10 +101,20 @@ sps.add_measurement('pm10', 'r(10, 200)', 2)
 
 sps.add_dependency('pm10', c.get_mesurments, 'pm10')
 sps.add_measurement('pm25', 'd.pm10*r(0.8, 1.2)', 2)
-
 c.add_sensor(sps)
-structure=['rssi','x','y', 'h', 'temperature', 'pressure', 'pm25', 'pm10']
-with serial.Serial('com10', 19200, timeout=1) as ser:
+
+mq9 = Sensor('MQ9')
+mq9.add_measurement('air_quality', 'r(0, 1000)', 0)
+c.add_sensor(mq9)
+
+dht22 = Sensor('DHT22')
+dht22.add_measurement('humidity', 'r(0, 100)', 0)
+c.add_sensor(dht22)
+
+
+
+structure=['rssi','send_num','x','y','h','pressure', 'temperature', 'air_quality', 'pm25', 'pm10','humidity','battery']
+with serial.Serial('com12', 19200, timeout=1) as ser:
     while(c.mesurments['h']>0):
         c.update()
         text=''
@@ -112,4 +124,4 @@ with serial.Serial('com10', 19200, timeout=1) as ser:
         print(text)
         #print(c.mesurments)
         ser.write(text.encode())
-        time.sleep(0.3)
+        time.sleep(0.5)
